d89d4b6d8a755888889dc9ef59f64d92cc23f5d5
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-01-27 15:19:58 +0100

    Use PyYAML

--- parse_entries.py
+++ parse_entries.py
@@ -2,25 +2,14 @@ import os
 import glob
 import json
 
+import yaml
+
 CATEGORIES = ['facilities', 'web_services', 'codes']
 
 TITLE = {}
 TITLE['facilities'] = "Observatories and Facilities"
 TITLE['web_services'] = "Online/Web Services"
 TITLE['codes'] = "Codes and Software"
-
-
-def parse_simple_yaml(filename):
-    content = {}
-    for line in open(filename):
-        try:
-            key, value = line.strip().split(':', 1)
-        except ValueError:
-            break
-        if len(value) == 0:
-            continue
-        content[key.strip()] = value.strip()
-    return content
 
 database = []
 
@@ -32,9 +21,13 @@ for category in CATEGORIES:
     category_database['content'] = {}
 
     for entry in glob.glob(os.path.join('entries', category, '*.yaml')):
-        content = parse_simple_yaml(entry)
+        with open(entry) as infile:
+            content = yaml.load(infile)
         short = os.path.splitext(os.path.basename(entry))[0]
-        category_database['content'][short] = content
+        category_database['content'][short] = {}
+        for key in content:
+            if content[key] is not None:
+                category_database['content'][short][key] = content[key]
 
     database.append(category_database)
 
