b0ab40478d22ab097504f1d0dba857c705acb952
Stuart <gravatar@strudel.org.uk>
2015-02-19 16:25:33 +0000

    Concatenate all items containing the phrase "...made use of"
    Deals with issue #15

--- functions.js
+++ functions.js
@@ -71,14 +71,18 @@ function show_acknowledgment(data) {
   var latex_checkbox = document.getElementById("option_latex");
   var facilities_checkbox = document.getElementById("option_facilities");
   
-  main_text = "<h2>Suggested acknowledgment</h2>";
-  bibtex_text = "<h2>BibTeX</h2>"
+  var main_text = "<h2>Suggested acknowledgment</h2>";
+  var bibtex_text = "<h2>BibTeX</h2>"
 
-  facilities = [];
+  var facilities = [];
+  var textA = [];
+  var textB = [];
+  var madeuseof = "made use of";
+  var delim = ";";
 
   for (var i = 0; i < data.length; i++) {
 
-    category = data[i]
+    category = data[i];
     
     var checkedBoxes = get_checked_boxes("checkbox_" + category.short);
     for (var j = 0; j < checkedBoxes.length; j++) {
@@ -95,21 +99,45 @@ function show_acknowledgment(data) {
       } else {
         text = category.content[checkedBoxes[j].value].text
       }
-      main_text += text + " ";
+      if (text.indexOf(madeuseof) > 0){
+      	tmp = text.substr(text.indexOf(madeuseof)+madeuseof.length);
+      	if(tmp[tmp.length-1]==".") tmp = tmp.substr(0,tmp.length-1)
+      	textA.push(tmp);
+      }else textB.push(text);
 
       if (facilities_checkbox.checked) {
         if (category.content[checkedBoxes[j].value].facilities) {
           facilities.push(category.content[checkedBoxes[j].value].facilities);
         }
       }
-
     }
-
   }
-
+  
   facilities_text = "<h2>Facilities</h2>"
   if(facilities.length > 0) {
     facilities_text += "\\textit{Facilities:}" + facilities.join(", ")
+  }
+  // Build the first part of the acknowledgements which
+  // concatenates the entries containing the phrase:
+  // "made use of".
+  if(textA.length > 0){
+    main_text += "This research made use of";
+    for(var i = 0; i < textA.length; i++){
+      // If this is not the first item, add a delimiter
+      if(i > 0) main_text += delim;
+      // Add a space
+      if(main_text[main_text.length-1] != " ") main_text += " ";
+      // Add the shortened text
+      main_text += textA[i];
+    }
+    main_text += ". "
+  }
+  if(textB.length > 0){
+    for(var i = 0; i < textB.length; i++){
+      // Add a space
+      if(i > 0 && main_text[main_text.length-1] != " ") main_text += " ";
+      main_text += textB[i];
+    }
   }
   document.getElementById("ack_main").innerHTML = main_text;
 
