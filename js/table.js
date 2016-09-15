d403b476f75438034db837b5b481a58b3913c361
Cédric <cedric@onekilopars.ec>
2016-09-15 09:42:17 +0200

    Refactoring but data.json will have to be reformatted

--- js/table.js
+++ js/table.js
@@ -1,54 +1,74 @@
 
 var checkboxes = [];
+var data = {};
 
-function build_table(data) {
-    content = "";
-    this.checkboxes = [];
+function accordionHeader(title, i) {
+    return '<div class="panel">' +
+        '<div class="panel-heading">' +
+        '   <h3 class="panel-title">' +
+        '       <a data-toggle="collapse" data-parent="#accordion" href="#accordion'+i+'">' +
+                    title +
+        '       </a>' +
+        '   </h3>' +
+        '</div>' +
+        '<div id="accordion'+i+'" class="panel-collapse collapse">' +
+        '   <div class="panel-body">';
+}
 
-    for (var i = 0; i < data.length; i++) {
+function accordionFooter() {
+    return '</div></div></div>';
+}
 
-        category = data[i];
+function build_table(filterString) {
 
-        var accordion = '<div class="panel">' +
-            '<div class="panel-heading">' +
-            '   <h3 class="panel-title">' +
-            '       <a data-toggle="collapse" data-parent="#accordion" href="#accordion'+i+'">' +
-                        category.title +
-            '       </a>' +
-            '   </h3>' +
-            '</div>' +
-            '<div id="accordion'+i+'" class="panel-collapse collapse">' +
-            '   <div class="panel-body">' +
-            '       <div class="table-responsive"><table class="table">';
+    checkboxes = [];
+    $.each(data, function(i, category) {
 
-        for (var short_name in category.content) {
+        var accordion = accordionHeader(category.title, i);
+        accordion += '<div class="table-responsive"><table class="table">';
+
+        var filteredEntries = category.content.filter(function(val) {
+            if (filterString === 'undefined') {
+                return true;
+            }
+            return val.toLowerCase().indexOf(filterString.toLowerCase()) != -1;
+        });
+
+        $.each(filteredEntries, function(j, entry) {
+
             accordion += '<tr><td>';
-            accordion += '<input type="checkbox" name="checkbox_' + category.short + '" id="' + category.short + '_' + short_name+ '" value="' + short_name + '"> ';
+            accordion += '<input type="checkbox" name="checkbox_' + category.short + '" id="' + category.short + '_' + entry+ '" value="' + entry + '"> ';
 
-            accordion += '&nbsp;';
-            if(typeof(category.content[short_name].url) != 'undefined') {
-                accordion += ' <a href="' + category.content[short_name].url + '">'
-                    + category.content[short_name].name + '\n'
-                    + '</a>\n';
+            if(typeof(entry.url) != 'undefined') {
+                accordion += ' <a href="' + entry.url + '">' + entry.name + '\n</a>\n';
             } else {
-                accordion += ' '+category.content[short_name].name + '\n';
+                accordion += ' ' + entry.name + '\n';
             }
 
             accordion += "</td></tr>";
 
             checkboxes.push({
-                'id': category.short + '_' + short_name,
-                'value': short_name,
-                'name': category.content[short_name].name
+                'id': category.short + '_' + entry,
+                'value': entry,
+                'name': entry.name
             });
-        }
+        });
 
-        accordion += "</table></div></div></div></div>";
+        accordion += "</table></div>";
+        accordion += accordionFooter();
 
-        $("#data-table > tbody:last-child").append(accordion);
-    }
+        $('#data-table > tbody:last-child').append(accordion);
+    });
 }
 
-$(document).ready(function(){
-    $.getJSON("database.json", build_table);
+$(document).ready(function() {
+    $.getJSON('database.json', function(jsonData) {
+        data = jsonData;
+        build_table($('#search-input').value);
+    });
+
+    $('#search-input').on('input', function(){
+        build_table($('#search-input').value);
+    });
+
 });
